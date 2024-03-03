7ba4aaf7d6a64495528d1b63c753b74187c11923
dvisner <dvisner@redroof.com>
2024-03-03 12:50:55 -0500

    changed colors

--- lua/dvisner/lazy/colors.lua
+++ lua/dvisner/lazy/colors.lua
@@ -1,20 +1,12 @@
-function ColorMyPencils(color)
-	color = color or "catppuccin"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
 return {
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
+		"maxmx03/solarized.nvim",
+		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd("colorscheme catppuccin")
+			vim.o.background = "light" -- or 'light'
 
-			ColorMyPencils()
+			vim.cmd.colorscheme("solarized")
 		end,
 	},
 }
