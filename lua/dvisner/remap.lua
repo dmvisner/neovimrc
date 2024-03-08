779d79367a611386a61eb73577956e3728e4ed3b
dvisner <dvisner@redroof.com>
2024-03-08 10:19:21 -0500

    couple more bindings for adding semicolons easily

--- lua/dvisner/remap.lua
+++ lua/dvisner/remap.lua
@@ -14,3 +14,13 @@ vim.keymap.set("n", "<leader>bp", ":bprev<CR>")
 vim.keymap.set("v", "<leader>y", '"+y')
 
 vim.keymap.set("n", "<leader>yy", '"+yy')
+
+function append_semicolon()
+	local current_line = vim.fn.line(".")
+	local line_length = #vim.fn.getline(current_line)
+	local col = line_length + 1
+	vim.api.nvim_buf_set_text(0, current_line - 1, col - 1, current_line - 1, col - 1, { ";" })
+	vim.fn.cursor(current_line, col)
+end
+vim.keymap.set("i", ";;", "<C-O>:lua append_semicolon()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>;;", "A;<Esc>", { noremap = true, silent = true })
